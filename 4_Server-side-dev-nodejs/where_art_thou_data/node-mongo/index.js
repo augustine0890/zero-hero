eb8ee043797594b84094dba5c3631ffe716e35b3
augustine <augustino0890@gmail.com>
2020-10-10 18:53:33 +0900

    MongoDB with Promises

--- 4_Server-side-dev-nodejs/where_art_thou_data/node-mongo/index.js
+++ 4_Server-side-dev-nodejs/where_art_thou_data/node-mongo/index.js
@@ -6,29 +6,31 @@ const url = 'mongodb://localhost:27017';
 const dbname = 'conFusion';
 
 
-MongoClient.connect(url, {useUnifiedTopology:true}, (err, client) => {
-    assert.strictEqual(err, null);
+MongoClient.connect(url, {useUnifiedTopology:true}).then((client) => {
     console.log('Connected correctly to server');
-
     const db = client.db(dbname);
-    dboper.insertDocument(db, { name: "Augustine", description: "Testing" }, 'dishes', (result) => {
-        console.log('Insert Document:\n', result.ops);
-        
-        dboper.findDocument(db, 'dishes', (docs) => {
+
+    dboper.insertDocument(db, { name: "Augustine", description: "Testing" }, 'dishes')
+        .then((result) => {
+            console.log('Insert Document:\n', result.ops);
+            return dboper.findDocument(db, 'dishes');
+        })
+        .then((docs) => {
             console.log('Found Documents:\n', docs);
-            
-            dboper.updateDocument(db, { name: "Augustine" }, { description: "Updated Testing" }, 'dishes', (result) => {
-                console.log('Updated Documents:\n', result.result);
-
-                dboper.findDocument(db, 'dishes', (docs) => {
-                    console.log('Found Documents:\n', docs);
-
-                    db.dropCollection('dishes', (result) => {
-                        console.log('Dropped Collection: ', result);
-                        client.close();
-                    });
-                });
-            });
-        });
-    });
-});
+            return dboper.updateDocument(db, { name: "Augustine" }, { description: "Updated Testing" }, 'dishes');
+        })
+        .then((result) => {
+            console.log('Updated Documents:\n', result.result);
+            return dboper.findDocument(db, 'dishes');
+        })
+        .then((docs) => {
+            console.log('Found Update Documents:\n', docs);
+            return db.dropCollection('dishes');
+        })
+        .then((result) => {
+            console.log('Dropped Collection: ', result);
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+})
+.catch((err) => console.log(err));
--- 4_Server-side-dev-nodejs/where_art_thou_data/nodeMongDB.md
+++ 4_Server-side-dev-nodejs/where_art_thou_data/nodeMongDB.md
@@ -7,4 +7,26 @@
     - Connecting to a MongoDB
     - Inserting, deleting, updating and querying documents
 
-## 
+## Callback Hell
+- Heavily nested callback code
+    - Results from our tendency to write code top to bottom
+    - Pyramid of doom
+- Can use promises to tame it
+    - Tries to preserve the top-down appearance of the code
+
+## Promise
+- Promise is a mechanism that supports asynchoronous computation
+- Proxy for a value not necessarily known when the promise is created:
+    - It represents a value that may be available now, or in the future, or never
+
+`new Promise (function (resolve, reject) {...});`
+- Solves the callback hell problem
+- Promises can be chained
+- Can immediately return:
+    - Promise.resolve(result)
+    - Promise.reject(error)
+
+## Consuming Promises
+- Comsumers of promise are notified of the fulfillment or rejection of the promise
+    - Register the callbacks to handle fulfillment and rejection with the `.then()` --> can be chained
+    - Use `.catch()` for handling errors
