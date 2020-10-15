6234b131e08f9ad26bb9fd1b1c922bfc75dd15a4
augustine <augustino0890@gmail.com>
2020-10-16 00:27:21 +0900

    Cookies

--- 4_Server-side-dev-nodejs/authentication/auth.md
+++ 4_Server-side-dev-nodejs/authentication/auth.md
@@ -9,3 +9,18 @@
     1. Username and password are combined into a string `"username:password"`
     2. The resulting string literal is then encoded using Base64.
     3. The authorization method and a space, i.e. "Basic" is then put before the encoded string.
+
+## Cookies, Tea and err... Express Sessions
+- HTTP Cookies: small piece of data sent from web server and stored on the client side
+- Each subsequent request from the client side should include the _cookie_ in the request header
+- Signed cookies: signed with a secret key on the server side
+    - Digital signature with key-hash message authentication code (verifiable)
+- Used to track user sessions
+    - Combination of cookies with session id and server-side storage of information indexed by session id
+    - Session information:
+        - Stored by default in-memory (wiped out when server restarts)
+        - Stored in permanent store on server side
+        - Distributed session store if using multiple replicated servers
+- Using cookies-parser
+    - The _cookies-parser_ Experss middleware is already included in the Express REST API application
+    - `npm install cookie-parser --save`
--- 4_Server-side-dev-nodejs/final/conFusionServer/app.js
+++ 4_Server-side-dev-nodejs/final/conFusionServer/app.js
@@ -16,14 +16,14 @@ const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url, {
-    useUnifiedTopology: true, 
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+		useUnifiedTopology: true, 
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false
 });
 
 connect.then((db) => {
-  console.log('Connected to server!');
+	console.log('Connected to server!');
 }, (err) => { console.log(err);});
 
 var app = express();
@@ -35,33 +35,46 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    
-    err.status = 401;
-    return next(err);
-  }
+	console.log(req.signedCookies);
 
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  
-  var username = auth[0];
-  var password = auth[1];
-
-  if (username === 'admin' && password === 'password') {
-    next();
-  } else {
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    
-    err.status = 401;
-    return next(err);
-  }
+	if (!req.signedCookies.user) {
+		var authHeader = req.headers.authorization;
+		if (!authHeader) {
+			var err = new Error('You are not authenticated!');
+			res.setHeader('WWW-Authenticate', 'Basic');
+			
+			err.status = 401;
+			return next(err);
+		}
+	
+		var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+		
+		var username = auth[0];
+		var password = auth[1];
+	
+		if (username === 'admin' && password === 'password') {
+			res.cookie('user', 'admin', { signed: true })
+			next();
+		} else {
+			var err = new Error('You are not authenticated!');
+			res.setHeader('WWW-Authenticate', 'Basic');
+			
+			err.status = 401;
+			return next(err);
+		}
+	} else {
+		if (req.signedCookies.user === 'admin') {
+			next();
+		} else {
+			var err = new Error('You are not authenticated!');
+			
+			err.status = 401;
+			return next(err);
+		}
+	}
 }
 app.use(auth);
 
@@ -75,18 +88,18 @@ app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
