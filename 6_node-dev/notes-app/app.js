2cd41026e3eeef32a3ce6fddaee7981c6d081c6c
augustine <augustino0890@gmail.com>
2020-10-21 23:53:23 +0900

    Importing npm modules

--- 6_node-dev/notes-app/app.js
+++ 6_node-dev/notes-app/app.js
@@ -1,5 +1,12 @@
+const validator = require('validator');
 const add = require('./utils');
 const getNotes = require('./notes');
 
+
 console.log(add(1, 2));
-console.log(getNotes('This is a note.'))
+console.log(getNotes('This is a note.'));
+
+console.log(validator.isEmail('augustine@example.com'));
+console.log(validator.isEmail('augustine.com'));
+console.log(validator.isURL('https://test.com'));
+console.log(validator.isURL('augustine@example.com'));
