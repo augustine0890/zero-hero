b1a0d5af3a58e01ad3349198cd95a943565d67aa
augustine <augustino0890@gmail.com>
2020-10-30 23:36:28 +0900

    Strings

--- 7_Java/Ultimate_java/src/com/augustine/Main.java
+++ 7_Java/Ultimate_java/src/com/augustine/Main.java
@@ -6,6 +6,7 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
+        // Primitive types
         byte myAge = 30;
         myAge = 29;
         int viewsCount = 123_456_789;
@@ -14,15 +15,23 @@ public class Main {
         char letter = 'A';
         boolean isExpire = false;
 
+        // Reference types
         Date now = new Date();
 //        now.getTime();
 
+        // Reference vs Primitive
         Point point1 = new Point(1, 1);
         Point point2 = point1;
         point1.x = 2;
 
+        // Strings
+        String message = new String(" Hello World" + "!");
 
-        System.out.println("Hello World");
+        System.out.println(message);
+        System.out.println(message.endsWith("!"));
+        System.out.println(message.indexOf("H"));
+        System.out.println(message.trim());
+
         System.out.println(myAge);
         System.out.println(viewsCount);
         System.out.println(biggerViewsCount);
