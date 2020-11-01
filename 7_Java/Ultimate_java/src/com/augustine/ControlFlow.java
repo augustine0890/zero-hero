519efaca83301487435bd0f2c5fe610423b3d046
augustine <augustino0890@gmail.com>
2020-11-01 23:59:29 +0900

    Comparison operators

--- 7_Java/Ultimate_java/src/com/augustine/ControlFlow.java
+++ 7_Java/Ultimate_java/src/com/augustine/ControlFlow.java
@@ -1,2 +1,12 @@
-package com.augustine;public class ControlFlow {
+package com.augustine;
+
+public class ControlFlow {
+    public static void main(String[] args) {
+        // Comparison Operators
+        int x = 1;
+        int y = 2;
+        System.out.println(x == y);
+        System.out.println(x != y);
+
+    }
 }
