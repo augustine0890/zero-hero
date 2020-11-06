e7329716429e335d0d4bcbcf43e2ff76da3770f1
augustine <augustino0890@gmail.com>
2020-11-06 22:29:26 +0900

    Break and continue statements

--- 7_Java/Ultimate_java/src/com/augustine/ControlFlow.java
+++ 7_Java/Ultimate_java/src/com/augustine/ControlFlow.java
@@ -69,17 +69,23 @@ public class ControlFlow {
 
         Scanner scanner = new Scanner(System.in);
         String input = "Initialize";
-        while (!input.equals("quit")) {
+        while (true) {
             System.out.println("Input: ");
             input = scanner.next().toLowerCase();
+            if (input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
             System.out.println(input);
         }
 
-        /* Do..While Loops */
+        /* Do..While Loops
         do {
             System.out.println("Input: ");
             input = scanner.next().toLowerCase();
             System.out.println(input);
         } while (!input.equals("quit"));
+
+         */
     }
 }
