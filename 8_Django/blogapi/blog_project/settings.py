2edb6b94b1e82016b42c08c69eb2eabe110e9ee4
augustine <augustino0890@gmail.com>
2021-01-05 15:34:21 +0900

    REST API Blog

--- 8_Django/blogapi/blog_project/settings.py
+++ 8_Django/blogapi/blog_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # 3rd-party apps
+    'rest_framework',
     # Local
     'posts.apps.PostsConfig',
 ]
@@ -121,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
--- 8_Django/blogapi/note.md
+++ 8_Django/blogapi/note.md
@@ -1,3 +1,12 @@
 # Blog API
 
 - `python manage.py createsuperuser` -> user/pw: `augustine/fitpet123`
+
+## REST Framework
+- `urls.py` file for the URL routes
+- `serializers.py` file to transform the data into JSON
+- `views.py` file to apply logic to each API endpoint
+
+- Install REST framework
+    - `pipenv install djangorestframework==3.11.0`
+    
